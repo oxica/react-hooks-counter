c3c451c11175915beec18af081a4b37f4f4bdf5d
Oksana <kskaplun@gmail.com>
2022-05-28 19:54:32 +0300

    refactoring to hooks

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,49 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import s from './Counter.module.css';
-import Controls from './Controls/Controls';
 
-class Counter extends Component {
-  static defaultProps = {
-    initialValue: 0,
-  };
+export default function Counter() {
+  const [count, setCount] = useState(0);
 
-  static propTypes = {
-    //
-  };
+  return (
+    <div className={s.counter}>
+      <span className={s.value}>{count}</span>
 
-  state = {
-    value: this.props.initialValue,
-  };
-
-  handleIncrement = () => {
-    // this.setState({ value: 123123 });
-    this.setState(prevState => {
-      return {
-        value: prevState.value + 1,
-      };
-    });
-  };
-
-  handleDecrement = () => {
-    this.setState(prevState => {
-      return {
-        value: prevState.value - 1,
-      };
-    });
-  };
-
-  render() {
-    return (
-      <div className={s.counter}>
-        <span className={s.value}>{this.state.value}</span>
-
-        <Controls
-          onIncrement={this.handleIncrement}
-          onDecrement={this.handleDecrement}
-        />
+      <div className={s.controls}>
+        <button type="button" onClick={() => setCount(state => state + 1)}>
+          -1
+        </button>
+        <button type="button" onClick={() => setCount(state => state - 1)}>
+          + 1
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Counter;
