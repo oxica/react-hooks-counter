33a595a638dddc793f222324e380bf0b5661e94b
Oksana <kskaplun@gmail.com>
2022-05-28 20:11:32 +0300

    use useReducer

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import s from './Counter.module.css';
 
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + action.payload };
+    case 'decrement':
+      return { ...state, count: state.count - action.payload };
+    default:
+      throw new Error(`Unsuported action type ${action.type}`);
+  }
+}
+
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
 
   return (
     <div className={s.counter}>
-      <span className={s.value}>{count}</span>
+      <span className={s.value}>{state.count}</span>
       <div className={s.controls}>
-        <button type="button" onClick={() => setCount(state => state + 1)}>
+        <button
+          type="button"
+          onClick={() => dispatch({ type: 'decrement', payload: 1 })}
+        >
           -1
         </button>
-        <button type="button" onClick={() => setCount(state => state - 1)}>
+        <button
+          type="button"
+          onClick={() => dispatch({ type: 'increment', payload: 1 })}
+        >
           + 1
         </button>
       </div>
